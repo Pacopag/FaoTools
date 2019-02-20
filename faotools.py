9ae99ae3b5edbed9ca14e3a5fe3589f527774afd
Chris Pagnutti <chris.pagnutti@gmail.com>
2019-02-20 15:20:20 -0500

    Utility functions to output crop items and elements

--- faotools.py
+++ faotools.py
@@ -24,6 +24,24 @@ def get_production_crop_item_codes():
         _production_crop_item_codes = db.production_crops.distinct(ITEM_CODE)
     return _production_crop_item_codes
 
+_production_crop_items = None
+def get_production_crop_items():
+    global _production_crop_items
+    if _production_crop_items is None:
+        _production_crop_items = db.production_livestock.aggregate([{'$group': {'_id': {'Item': '$Item', 'Item Code': '$Item Code'}}}])
+        _production_crop_items = [d['_id'] for d in _production_crop_items]
+        _production_crop_items = sorted(_production_crop_items, key=lambda x: x['Area'])
+    return _production_crop_items
+
+_production_crop_elements = None
+def get_production_crop_elements():
+    global _production_crop_elements
+    if _production_crop_elements is None:
+        _production_crop_elements = db.production_livestock.aggregate([{'$group': {'_id': {'Element': '$Element', 'Element Code': '$Element Code'}}}])
+        _production_crop_elements = [d['_id'] for d in _production_crop_elements]
+        _production_crop_elements = sorted(_production_crop_elements, key=lambda x: x['Area'])
+    return _production_crop_elements
+
 _production_livestock_primary_item_codes = None
 def get_production_livestock_primary_item_codes():
     global _production_livestock_primary_item_codes
